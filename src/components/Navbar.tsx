310ff09c135da7334a0abd8c524f071677fc56c6
Ayodejioladimeji <layobright11@gmail.com>
2022-08-21 17:49:04 +0100

    added toggle mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 // import logo
-import logo from "../assets/logo2.png";
+import logo from "../assets/logo.png";
+import logos from "../assets/logo2.png";
 import { Link } from "react-scroll";
+import Toggle from "./../common/Toggle";
+
+//
+// Get theme from localstorage
+const getStorageTheme = () => {
+  let theme: any = "light-theme";
+
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+
+  return theme;
+};
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(getStorageTheme());
+
   return (
     <div className="nav">
       <div className="nav-brand">
-        <img src={logo} alt="brand-logo" />
+        {theme === "light-theme" ? (
+          <img src={logo} alt="brand-logo" />
+        ) : (
+          <img src={logos} alt="brand-logo" />
+        )}
       </div>
 
       <div className="nav-button">
+        <Toggle setTheme={setTheme} theme={theme} />
+
         <Link
           to="newsletter"
           spy={true}
